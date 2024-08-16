d9c753ff8d4dda79377ede79dbef966676207912
safwaahmad <sahmedsaffi@gmail.com>
2024-08-17 01:47:17 +0500

    add

--- app.py
+++ app.py
@@ -1,28 +1,30 @@
 import streamlit as st
+import transformers
 from transformers import VitsModel, AutoTokenizer
 import torch
 import numpy as np
 import io
 import soundfile as sf
+
 # Load model and tokenizer
 model = VitsModel.from_pretrained("facebook/mms-tts-eng")
 tokenizer = AutoTokenizer.from_pretrained("facebook/mms-tts-eng")
 
 def generate_speech(text):
-    inputs = tokenizer(text, return_tensors="pt")
+  inputs = tokenizer(text, return_tensors="pt")
 
-    with torch.no_grad():
-        output = model(**inputs).waveform
+  with torch.no_grad():
+    output = model(**inputs).waveform  # Corrected typo: waveform (not waveformform)
 
-    # Convert the waveform tensor to a NumPy array
-    waveform = output.squeeze().cpu().numpy()
+  # Convert the waveform tensor to a NumPy array
+  waveform = output.squeeze().cpu().numpy()
 
-    # Convert the waveform to bytes
-    audio_bytes_io = io.BytesIO()
-    sf.write(audio_bytes_io, waveform, samplerate=22050, format='WAV')
-    audio_bytes_io.seek(0)
+  # Convert the waveform to bytes
+  audio_bytes_io = io.BytesIO()
+  sf.write(audio_bytes_io, waveform, samplerate=22050, format='WAV')
+  audio_bytes_io.seek(0)
 
-    return audio_bytes_io
+  return audio_bytes_io
 
 # Streamlit UI
 st.title("Text-to-Speech Converter")
@@ -33,11 +35,11 @@ st.write("Enter text below and click 'Generate Speech' to convert it to audio.")
 text_input = st.text_area("Text to convert:", "Some example text in the English language")
 
 if st.button("Generate Speech"):
-    if text_input:
-        st.write("Generating speech...")
-        audio_bytes_io = generate_speech(text_input)
+  if text_input:
+    st.write("Generating speech...")
+    audio_bytes_io = generate_speech(text_input)
 
-        # Display audio in Streamlit
-        st.audio(audio_bytes_io, format="audio/wav")
-    else:
-        st.write("Please enter some text.")
+    # Display audio in Streamlit
+    st.audio(audio_bytes_io, format="audio/wav")
+  else:
+    st.write("Please enter some text.")
